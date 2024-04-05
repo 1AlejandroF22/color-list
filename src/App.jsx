4cb439370a4107b3ccb9dfc59d8ae9c901c2b359
Didier Alejandro Fajardo Annear <didier.fajardo@uptc.edu.co>
2024-04-04 22:48:49 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ColorList from "./components/ColorList";
+
+import "./index.css";
+
+const App = () => (
+	<>
+		<ColorList />
+  </>
+);
+ReactDOM.createRoot(document.getElementById("app")).render(<App/>);
